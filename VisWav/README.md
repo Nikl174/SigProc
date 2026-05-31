1541c23cb62c1050fce9d0a07f54462264fa4571
nikl <nikl174@mailbox.org>
2026-05-31 02:33:11 +0200

    VisWav: improved plotting program and Readme and added spectrum visualisation

--- VisWav/README.md
+++ VisWav/README.md
@@ -3,23 +3,62 @@
 - Fourier transform signal and visualise with [SDL3](https://github.com/libsdl-org/SDL)
 - also using Emscripten to compile it so WASM
 
-## Usage
+## Dependencies
+
+- SDL3
+- fftw3
+- cmake
+
+### Build
+
+- cmake
+
 ```bash
-make # native using gcc
-make emscripten # web using emcc
+mkdir build
+cd build
+cmake ..
+make
 ```
+- just gcc
+
+```bash
+gcc src/fft.c src/plot.c src/read_wav.c -I include -lSDL3 -lm -lfftw3 -o VisWav
+```
+
+## Usage
+
+```bash
+$: ./build/VisWav -h
+Prints the WAV file samples
+Usage: ./build/VisWav [OPTIONS] PATH_TO_WAV_FILE
+OPTIONS:
+        -s/--spectrum:  plot the spectrum instead
+        -h/--help:      print this help
+        -v/--verbose:   print additional info
+        -p/--print:     print plotted values
+```
+
+
 ## [readWAV](./src/readWAV.c)
 
+```bash
+$: ./build/VisWav ./example_wav/Sin440.wav
+```
 ![Sin WAV plot](./img/wav_sin_plot_sdl.png)
 
 - simple program for reading and visualising a WAV file
 - Mouse wheel to zoom
 
+## [fft spectrum](./src/fft.c)
 ```bash
-gcc readWAV.c -lSDL3 -lm -o readWAV
+$: ./build/VisWav ./example_wav/Sin440_1000.wav
 ```
+![Sin 440 and 1000 Spectrum Plot](./img/wav_spectrum_plot.png)
+
+- also possible to show the signals spectrum
 
 ## Notes
 
 - [WAV Header format](https://en.wikipedia.org/wiki/WAV:~:text=WAV%20file%20header)
 - [RIFF and WAV reference (IBM/Microsoft)](https://www.aelius.com/njh/wavemetatools/doc/riffmci.pdf)
+- [fftw tutorial](https://www.github.com/jonathanschilling/fftw_tutorial/)
--- VisWav/src/plot.c
+++ VisWav/src/plot.c
@@ -1,6 +1,7 @@
 #include "fft.h"
 #include "read_wav.h"
 #include <SDL3/SDL.h>
+#include <getopt.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -8,7 +9,7 @@
 #include <stdlib.h>
 
 #define WINDOW_WIDTH 1920
-#define WINDOW_HEIGHT 800
+#define WINDOW_HEIGHT 900
 
 // Convert data coordinates to screen coordinates
 void to_screen(float x, float y, int *sx, int *sy, float xmin, float xmax,
@@ -17,9 +18,9 @@ void to_screen(float x, float y, int *sx, int *sy, float xmin, float xmax,
   *sy = (int)(WINDOW_HEIGHT - (y - ymin) / (ymax - ymin) * WINDOW_HEIGHT);
 }
 
-void plot_wav_file(SDL_Renderer *renderer, int32_t *samples, uint8_t channels,
-                   int width, int heigth, float xmin, float xmax, float ymin,
-                   float ymax) {
+void plot_points(SDL_Renderer *renderer, int32_t *samples, uint8_t channels,
+                 int width, int heigth, float xmin, float xmax, float ymin,
+                 float ymax) {
 
   // Draw axes
   SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
@@ -40,53 +41,7 @@ void plot_wav_file(SDL_Renderer *renderer, int32_t *samples, uint8_t channels,
   }
 }
 
-int main(int argc, char *argv[]) {
-
-  RIFF_Header header;
-  int read_samples = 0;
-  uint16_t x_max_divisor = 400;
-  int32_t *samples;
-  int32_t *spectrum;
-  int fft_samples_num = 880;
-
-  if (argc > 1) {
-    samples = parse_RIFF_file(argv[1], &header, &read_samples);
-    fft_samples_num = header.frequency / 2;
-    print_riff_header(&header);
-    printf("-----------------------\n");
-    printf("Read samples: %i\n", read_samples);
-    if (!(samples && read_samples > fft_samples_num)) {
-      printf("Error reading samples: %x, fft_samples_num: %d\n", samples,
-             fft_samples_num);
-      return 1;
-    }
-    // TODO
-    double *d_samples = calloc(sizeof(double), fft_samples_num);
-    spectrum = calloc(sizeof(int32_t), fft_samples_num);
-    for (int i = 0; i < fft_samples_num; i++) {
-      d_samples[i] = (double)samples[i];
-    }
-
-    SpectrumPoint *points =
-        to_spectrum(d_samples, fft_samples_num, header.frequency);
-
-    if (!points) {
-      printf("Error while calculating the spectrum!\n");
-      return 1;
-    }
-
-    printf("Spectrum:\n");
-    printf("-----------------\n");
-    for (int i = 0; i < fft_samples_num / 2; i++) {
-      spectrum[i] = points[i].magnitude;
-      printf("Mag: %f;\tFreq: %f;\n", points[i].magnitude, points[i].frequency);
-    }
-    printf("-----------------\n");
-
-  } else {
-    printf("Usage: %s PATH_TO_WAV_FILE\n", argv[0]);
-    return 1;
-  }
+void sdl_points_plot(int32_t *points, uint size, uint channels) {
 
   // SDL INIT
   SDL_Init(SDL_INIT_VIDEO);
@@ -100,10 +55,8 @@ int main(int argc, char *argv[]) {
   SDL_Event event;
 
   // Plot bounds
-  float xmin = 0,
-        xmax = fft_samples_num; // header.frequency / 2 *
-                                // header.num_channels / x_max_divisor;
-  float ymin = -INT32_MAX, ymax = INT32_MAX;
+  float xmin = 0, xmax = size / 2;
+  float ymin = -INT32_MAX - 1, ymax = INT32_MAX;
 
   while (running) {
     while (SDL_PollEvent(&event)) {
@@ -114,8 +67,8 @@ int main(int argc, char *argv[]) {
       if (event.type == SDL_EVENT_MOUSE_WHEEL) {
         float zoomFactor = (event.wheel.y > 0) ? 0.9f : 1.1f;
         xmax *= zoomFactor;
-        if (xmax >= read_samples) {
-          xmax = read_samples - 1;
+        if (xmax >= size) {
+          xmax = size - 1;
         }
       }
     }
@@ -123,18 +76,144 @@ int main(int argc, char *argv[]) {
     SDL_SetRenderDrawColor(renderer, 20, 20, 20, 255);
     SDL_RenderClear(renderer);
 
-    plot_wav_file(renderer, spectrum, header.num_channels, WINDOW_WIDTH,
-                  WINDOW_HEIGHT, xmin, (xmax / 2) + 1, ymin, ymax);
+    plot_points(renderer, points, channels, WINDOW_WIDTH, WINDOW_HEIGHT, xmin,
+                xmax, ymin, ymax);
 
     SDL_RenderPresent(renderer);
     SDL_Delay(16);
   }
 
-  free(samples);
-  samples = NULL;
-
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
-  return 0;
+}
+
+void print_help(char *argv[]) {
+  printf("Prints the WAV file samples\n");
+  printf("Usage: %s [OPTIONS] PATH_TO_WAV_FILE\n", argv[0]);
+  printf("OPTIONS:\n");
+  printf("\t-s/--spectrum:\tplot the spectrum instead \n");
+  printf("\t-h/--help:\tprint this help\n");
+  printf("\t-v/--verbose:\tprint additional info \n");
+  printf("\t-p/--print:\tprint plotted values\n");
+}
+
+int main(int argc, char *argv[]) {
+
+  RIFF_Header header;
+  int read_samples = 0;
+  uint16_t x_max_divisor = 400;
+  int32_t *samples;
+  int32_t *spectrum;
+  int fft_samples_num = 880;
+  bool use_spectrum = false;
+  bool print_values = false;
+  bool verbose = false;
+
+  int choice;
+  while (1) {
+    static struct option long_options[] = {
+        /* Use flags like so:
+        {"verbose", no_argument,  &verbose_flag, 'V'}*/
+        /* Argument styles: no_argument, required_argument, optional_argument */
+        {"verbose", no_argument, 0, 'v'},
+        {"help", no_argument, 0, 'h'},
+        {"spectrum", no_argument, 0, 's'},
+        {"print", no_argument, 0, 'p'},
+        {0, 0, 0, 0}};
+
+    int option_index = 0;
+
+    /* Argument parameters:
+      no_argument: " "
+      required_argument: ":"
+      optional_argument: "::" */
+
+    choice = getopt_long(argc, argv, "vhsp", long_options, &option_index);
+
+    if (choice == -1)
+      break;
+
+    switch (choice) {
+    case 'v':
+      verbose = true;
+      break;
+
+    case 'h':
+      print_help(argv);
+      return EXIT_FAILURE;
+      break;
+
+    case 's':
+      use_spectrum = true;
+      break;
+
+    case 'p':
+      print_values = true;
+      break;
+
+    case '?':
+      /* getopt_long will have already printed an error */
+      break;
+
+    default:
+      /* Not sure how to get here... */
+      return EXIT_FAILURE;
+    }
+  }
+
+  /* Deal with non-option arguments here */
+  if (optind < argc) {
+    samples = parse_RIFF_file(argv[optind], &header, &read_samples);
+    fft_samples_num = header.frequency / 2;
+    if (verbose)
+      print_riff_header(&header);
+    if (verbose)
+      printf("-----------------------\n");
+    if (verbose)
+      printf("Read samples: %i\n", read_samples);
+    if (!(samples && read_samples > fft_samples_num)) {
+      printf("Error reading samples: %x, fft_samples_num: %d\n", samples,
+             fft_samples_num);
+      return 1;
+    }
+    // TODO
+    double *d_samples = calloc(sizeof(double), fft_samples_num);
+    spectrum = calloc(sizeof(int32_t), fft_samples_num);
+    for (int i = 0; i < fft_samples_num; i++) {
+      d_samples[i] = (double)samples[i];
+      if (print_values)
+        printf("Sample %i: %d\n", i, samples[i]);
+    }
+
+    if (use_spectrum) {
+      SpectrumPoint *points =
+          to_spectrum(d_samples, fft_samples_num, header.frequency);
+
+      if (!points) {
+        printf("Error while calculating the spectrum!\n");
+        return 1;
+      }
+
+      for (int i = 0; i < fft_samples_num / 2; i++) {
+        spectrum[i] = points[i].magnitude;
+        if (print_values)
+          printf("Mag: %f;\tFreq: %f;\n", points[i].magnitude,
+                 points[i].frequency);
+      }
+      sdl_points_plot(spectrum, fft_samples_num, 1);
+    } else {
+      sdl_points_plot(samples, fft_samples_num, header.num_channels);
+    }
+
+    free(d_samples);
+  } else {
+    printf("Error: missing file!\n");
+    print_help(argv);
+    return EXIT_FAILURE;
+  }
+
+  free(spectrum);
+  free(samples);
+  return EXIT_SUCCESS;
 }
